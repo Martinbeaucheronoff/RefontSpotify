0fd10516502f981e8f5ae272e80f177b4f0c8dbd
MartinBeaucheron <beaucheronmartin@gmail.com>
2024-11-15 07:58:21 +0100

    inclusion des component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FooterComponent} from './footer/footer.component';
+import {LecteurComponent} from './lecteur/lecteur.component';
+import {HeaderComponent} from './header/header.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, LecteurComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
