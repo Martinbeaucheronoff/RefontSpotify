d4601cf43a24751824464bbf5e8721c850749e85
MartinBeaucheron <beaucheronmartin@gmail.com>
2024-11-15 12:00:08 +0100

    header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatFormFieldModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
