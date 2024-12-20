71ff3a8f7c5e1dfdb96c1ab331279a417ed4b40b
Himely <teodebaypro@gmail.com>
2024-12-20 17:15:43 +0100

    Update footer.component.ts

--- spotify-clone/src/app/footer/footer.component.ts
+++ spotify-clone/src/app/footer/footer.component.ts
@@ -3,9 +3,6 @@ import {MatToolbar} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-footer',
-  imports: [
-    MatToolbar
-  ],
   templateUrl: './footer.component.html',
   standalone: true,
   styleUrl: './footer.component.css'
